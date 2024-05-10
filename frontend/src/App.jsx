4a9de9d1d29ee4620e59f7c72d4b512810147e49
ship07-blip <kumarishivangi65782@gmail.com>
2024-05-11 05:15:03 +0530

    Added: Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./Components/Sidebar.jsx/Sidebar";
 
 function App() {
   return (
     <>
+      <Sidebar />
       <h1>Hello World</h1>
     </>
   );
