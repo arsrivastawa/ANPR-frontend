c43237cc3e4b48674375e064403ac29c70d37b3e
withnikhil <nikhil21907@gmail.com>
2024-05-11 05:20:09 +0530

    added dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
     <>
+      <Dashboard />
       <h1>Hello World</h1>
     </>
   );
