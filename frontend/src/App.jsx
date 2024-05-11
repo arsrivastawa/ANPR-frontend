30e62eca405706c32111a2eeb44c92082b511043
withnikhil <nikhil21907@gmail.com>
2024-05-11 13:58:48 +0530

    Added: Navbar and a timer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import Sidebar from "./Components/Sidebar.jsx/Sidebar";
 import Dashboard from "./Components/Dashboard";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
       <Sidebar />
       <Dashboard />
-      <h1>Hello World</h1>
+      <Navbar />
     </>
   );
 }
