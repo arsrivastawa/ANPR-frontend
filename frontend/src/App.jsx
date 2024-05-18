728e1371fc892e757857fea62eecd9a489b47afc
ship07-blip <kumarishivangi65782@gmail.com>
2024-05-19 04:33:55 +0530

    Added: Settings Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,9 @@ import Navbar from "./components/Dashboard/Navbar";
 import Footer from "./components/Footer/Footer";
 // import VideoStream from "./components/VideoStreamer/VideoStreamer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LiveFeed from "./pages/LiveFeed";
+import LiveFeed from "./pages/LiveFeed/LiveFeed";
+import Settings from "./pages/Settings/Settings";
+//import LiveFeed from "./pages/LiveFeed";
 
 
 function App() {
@@ -17,10 +19,11 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<LiveFeed />} />
+
+          < Route path = '/settings' element={<Settings />}/>
         </Routes>
         <Footer />
       </Router>
-      {/* <VideoStream /> */}
     </>
   );
 }
---
+++ frontend/src/pages/LiveFeed/LiveFeed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LiveFeed() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default LiveFeed
