9939359760cf7362a78d90140faacae38c8c8410
Aditya Ranjan <arsrivastawa2710@gmail.com>
2024-05-19 04:45:21 +0530

    Added: Four Camera Streams

--- frontend/src/components/CameraStream/CameraStream.jsx
+++ frontend/src/components/CameraStream/CameraStream.jsx
@@ -3,10 +3,17 @@ import io from "socket.io-client";
 
 const socket = io("http://localhost:5000");
 const CameraStream = () => {
-
   const [image, setImage] = useState("");
 
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
+  const [show3, setShow3] = useState(false);
+  const [show4, setShow4] = useState(false);
+
+
+  
   useEffect(() => {
+    socket.emit("start");
     socket.on("frame", (data) => {
       setImage(`data:image/jpeg;base64,${data}`);
     });
@@ -18,14 +25,123 @@ const CameraStream = () => {
   }, []);
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
-      <img src={image} style={{ width: "55%" }} />
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "8px",
+        width: "100%",
+      }}
+    >
+      <div className="w-4/5 h-[40vh] flex flex-row justify-center gap-2">
+        {show1 ? (
+          <img
+            src={image}
+            className="border border-slate-500"
+            style={{ width: "50%" }}
+          />
+        ) : (
+          <>
+            <div
+              style={{
+                width: "50%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "white",
+                flexDirection: "column",
+                backgroundColor: "black",
+              }}
+            >
+              <i class="text-8xl bi bi-camera-video-off-fill"></i>
+              <div>Please, Turn on the Video</div>
+            </div>
+          </>
+        )}
+        {show2 ? (
+          <img
+            src={image}
+            className="border border-slate-500"
+            style={{ width: "50%" }}
+          />
+        ) : (
+          <>
+            <div
+              style={{
+                width: "50%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "white",
+                flexDirection: "column",
+                backgroundColor: "black",
+              }}
+            >
+              <i class="text-8xl bi bi-camera-video-off-fill"></i>
+              <div>Please, Turn on the Video</div>
+            </div>
+          </>
+        )}
+      </div>
+      <div className="w-4/5 h-[40vh] flex flex-row justify-center gap-2">
+        {show3 ? (
+          <img
+            src={image}
+            className="border border-slate-500"
+            style={{ width: "50%" }}
+          />
+        ) : (
+          <>
+            <div
+              style={{
+                width: "50%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "white",
+                flexDirection: "column",
+                backgroundColor: "black",
+              }}
+            >
+              <i class="text-8xl bi bi-camera-video-off-fill"></i>
+              <div>Please, Turn on the Video</div>
+            </div>
+          </>
+        )}
+        {show4 ? (
+          <img
+            src={image}
+            className="border border-slate-500"
+            style={{ width: "50%" }}
+          />
+        ) : (
+          <>
+            <div
+              style={{
+                width: "50%",
+                height: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                color: "white",
+                flexDirection: "column",
+                backgroundColor: "black",
+              }}
+            >
+              <i class="text-8xl bi bi-camera-video-off-fill"></i>
+              <div>Please, Turn on the Video</div>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
 
 export default CameraStream;
-
 // src/VideoStream.js
 // import React, { useEffect, useState } from 'react';
 // import io from 'socket.io-client';
--- frontend/src/components/CameraStream/CameraStreamContainer.jsx
+++ frontend/src/components/CameraStream/CameraStreamContainer.jsx
@@ -2,34 +2,13 @@ import React, { useState } from "react";
 import CameraStream from "./CameraStream";
 
 function CameraStreamContainer() {
-  const [view, setView] = useState(false);
-
   return (
     <div style={{ display: "flex", flexDirection: "column", width: "100%" }}>
       <div className="filter-btn">
         <button onClick={() => setView(!view)}>Toggle Camera</button>
       </div>
-      {view ? (
-        <CameraStream />
-      ) : (
-        <>
-          <div
-            style={{
-              width: "55%",
-              height: "80%",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              color: "white",
-              flexDirection: "column",
-              backgroundColor: "black",
-            }}
-          >
-            <i class="text-8xl bi bi-camera-video-off-fill"></i>
-            <div>Please, Turn on the Video</div>
-          </div>
-        </>
-      )}
+
+      <CameraStream />
     </div>
   );
 }
